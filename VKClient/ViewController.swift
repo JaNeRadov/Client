968680d419826c2a439616cbcfcdd24cce088387
Yuriy Zarubin <temisfucincrazy@icloud.com>
2022-06-28 16:27:02 +0300

    Initial Commit

---
+++ VKClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKClient
+//
+//  Created by Jane Z. on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
